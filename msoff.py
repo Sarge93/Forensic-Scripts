0fe01bc72065c273b794955f583ff2c8063e4224
Sarge93 <sarge93@yandex.ru>
2021-09-04 23:13:06 +0300

    Update msoff.py

--- msoff.py
+++ msoff.py
@@ -1,11 +1,11 @@
 import msoffcrypto
 
-encrypted = open("C:\\Users\\sarge\\Desktop\\c38-xlm-macros\\c38-xlm-macros\\sample1-fb5ed444ddc37d748639f624397cff2a.bin", "rb")
+encrypted = open("C:\\Users\\John Doe\\Desktop\\c38-xlm-macros\\c38-xlm-macros\\sample1-fb5ed444ddc37d748639f624397cff2a.bin", "rb")
 file = msoffcrypto.OfficeFile(encrypted)
 
 file.load_key(password="VelvetSweatshop")  
 
-with open("C:\\Users\\sarge\\Desktop\\decrypted.bin", "wb") as f:
+with open("C:\\Users\\John Doe\\Desktop\\decrypted.bin", "wb") as f:
     file.decrypt(f)
 
 encrypted.close()
